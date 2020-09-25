0b4582842a262bb66172d26e8e00ec08aa489d8d
waterproofpatch <waterproofpatch@gmail.com>
2020-09-24 23:11:10 -0400

    ARRAY_LEN; main updates

---
+++ src/array_utils.h
@@ -0,0 +1,9 @@
+#ifndef _ARRAY_UTILS_H_
+#define _ARRAY_UTILS_H_
+
+/**
+ * @brief count the number of elements in an array.
+ */
+#define ARRAY_LEN(arr) (sizeof(arr) / sizeof(arr[0]))
+
+#endif
--- src/main.c
+++ src/main.c
@@ -3,15 +3,35 @@
  * @brief driver for the macros
  */
 #include <stdio.h>
+#include <assert.h>
 
-#include "logger.h"     /* LOG_* macros */
-#include "breakpoint.h" /* BREAKPOINT_* */
+#include "logger.h"      /* LOG_* macros */
+#include "breakpoint.h"  /* BREAKPOINT_* */
+#include "array_utils.h" /* BREAKPOINT_* */
 
+typedef struct SomeStruct
+{
+    int a;
+    int b;
+} SomeStruct_t;
+
+/**
+ * @brief Test some macro stuff
+ *
+ * @param argc count of CLI args
+ * @param argv ptr to array of CLI args
+ * @return int 0 on success
+ */
 int main(int argc, char **argv)
 {
+    SomeStruct_t someStructs[7];
+
+    assert(ARRAY_LEN(someStructs) == 7);
+
     LOG_DEBUG("A debug message\n");
     LOG_ERROR("An error message\n");
     LOG_INFO("An info message\n");
+
     BREAKPOINT();
     return 0;
 }
